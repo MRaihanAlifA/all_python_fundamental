6b776446df9080771d8f4a64db0b9bcb6f4d5586
MRaihanAlifA <muhammadraihanalif@gmail.com>
2022-02-18 07:57:52 +0700

    adding new code

--- data_list.py
+++ data_list.py
@@ -44,3 +44,57 @@ book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
 book_list.pop(-1)
 for i in range(0, len(book_list)):
     print(book_list[i])
+
+print('\ndeleting list using list comprehension')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+del book_list[1]
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\ndeleting list using list comprehension with start and end')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+del book_list[0:4]  #start:end
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\ndeleting list using list comprehension with start, end and step')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+del book_list[0:5:2]  #start:end:step
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nmaking new list using comprehension')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+new_book_list = book_list[:]
+del book_list
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
+
+print('\nmaking new list using comprehension')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+new_book_list = book_list[:]
+del book_list
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
+
+print('\nmaking new list using comprehension: odd only')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+new_book_list = book_list[0::2]
+del book_list
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
+
+print('\nmaking new list using comprehension: even only')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+new_book_list = book_list[1::2]
+del book_list
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
+
+print('\nmaking new list using comprehension: odd only without last book')
+book_list = ['mathematics', 'science', 'chemics', 'english', 'economy']
+new_book_list = book_list[0:-1:2]
+del book_list
+for i in range(0, len(new_book_list)):
+    print(new_book_list[i])
+
